50722a6ea38242fea6afb0789e996b32ab4686fd
Sophia Karski <sophiakarski@gmail.com>
2022-09-23 20:31:13 +0100

    Lecture 5

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
-// IIFE = Immediately Invoked Function Expression = ()();
-// PROTECT from bleeding local variables into global scope
-
 (function () {
-
-// enable certain things to PROTECT us from making mistakes
 'use strict';
-
-// [] = dependencies
-// must bind 'myFirstApp' module (AngularJS app) to .html
 
 angular.module('myFirstApp', [])
 
-// the way we define our ViewModel of View
-// Name of ViewModel/Controller, and takes function that defines functionallity of Controller
-// Now, bind this to View
-
-// CONTROLLER = VIEWMODEL
 .controller('MyFirstController', function ($scope) {
-$scope.name = "Sophia";
-
-$scope.sayHello = function () {
+  $scope.name = "Yaakov";
+  $scope.sayHello = function () {
     return "Hello Coursera!";
-};
-
+  };
 });
 
 })();
